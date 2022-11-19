8b3ccc5b566748231870fcf1b233f4a3bdf3de58
MubashirM-Ibrahim <officialmubashiribrahim77@gmail.com>
2022-11-19 11:31:51 +0500

    02-variables-in-js

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log("Lecture 2 Variabes In JS ");
+
+// /////////////////////////////////////////////////////////////////
+
+// Rules In javascript For Setting a Varaibles .
+// A variables is like a container . which is stored in our system memory .
+
+// jdhfjsdhfasjkhfconsole.log("Lecture 2 Variabes In JS "); Error !!!
+// sfdfsdfasf   Eroor !!!
+// 738472983   Eroor !!!
+
+// ////////////////////////////////////////////////////////////////////////////////
+
+// Variables Rules in Js 
+
+// Any Variable Not Start With Number . e.g let 89num = 90 ;  !!! Error !!! Not Allowed .   
+// Reserved Varibale names Not Used in JS . e.g let var = 90 ;  !!!  Error !!! Not Allowed .   
+// JS  is a case Sensitive Language .
+// We use 99, "string" , "string_90" ,    
+// let a = 67 
+//  let b = "string"
+// let string = "string78"
+// let string = "string_89"
+
+let num = 67 ;
+let nul = null ;
+let bigInteger = BigInt(90) ;
+let bool  = true ;
+let strigs = "Mubahsir" ;
+let symbols = Symbol()
+let undef = undefined ;
+
